30596e23d6e882279db52cc25536c9e0de836847
Exequiel Schiavo <exelu1495@gmail.com>
2024-05-04 15:32:48 -0300

    add: middleware para registrar solicitudes

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
+// Importa los módulos necesarios
 import express, { json } from 'express'
 import cors from "cors";
-
 import { dbconnect } from './src/db/db.js';
 import { employeesRouter } from './src/routers/employesRouter.js';
-import {routerRegistro} from './src/routers/registroRouter.js';
+import { routerRegistro } from './src/routers/registroRouter.js';
 
-
-
-
+// Crea la instancia de la aplicación Express
 const app = express();
 
 // Middleware para analizar el cuerpo de las solicitudes
-
 app.use(cors({
   origin: ['http://localhost:5173', 'http://192.168.164.214:5173'],
   methods: ['GET', 'POST','PATCH','DELETE'], 
@@ -19,24 +16,31 @@ app.use(cors({
 }));
 app.use(json());
 
+// Middleware para registrar las solicitudes entrantes
+app.use((req, res, next) => {
+  console.log(`Solicitud ${req.method} a ${req.path}`);
+  next();
+});
 
-// Usar el router para gestionar las rutas relacionadas con el escaneo
-app.use(employeesRouter );
-app.use(routerRegistro );
+// Usar el router para gestionar las rutas relacionadas con los empleados
+app.use(employeesRouter);
 
+// Usar el router para gestionar las rutas relacionadas con el registro
+app.use(routerRegistro);
 
-
+// Puerto en el que se ejecutará el servidor
 const PORT = process.env.PORT || 3000;
 
-const startServer = async ()  => {
+// Función para iniciar el servidor
+const startServer = async () => {
+  // Conexión a la base de datos
+  await dbconnect();
 
-  await dbconnect()
-
-
-   app.listen(PORT ,() => {
-  console.log(`Servidor en ejecución en el puerto ${PORT}`);
-});
+  // Iniciar el servidor en el puerto especificado
+  app.listen(PORT, () => {
+    console.log(`Servidor en ejecución en el puerto ${PORT}`);
+  });
 }
 
-
-startServer()
+// Iniciar el servidor
+startServer();
