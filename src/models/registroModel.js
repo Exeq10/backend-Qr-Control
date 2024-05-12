8a6824f7f58e5d309cd5b39cf176d84687e57a76
Exequiel Schiavo <exelu1495@gmail.com>
2024-05-12 20:46:41 -0300

    updateregisterModel

--- src/models/registroModel.js
+++ src/models/registroModel.js
@@ -1,6 +1,8 @@
 // registroModel.js
 import mongoose from "mongoose";
 
+import mongoose from 'mongoose';
+
 const registroSchema = new mongoose.Schema({
   usuario: {
     type: mongoose.Schema.Types.ObjectId,
@@ -12,9 +14,21 @@ const registroSchema = new mongoose.Schema({
     enum: ["entrada", "salida", "descanso"], // Agregar el tipo de marca "descanso"
     required: true
   },
-  marcaTiempo: { type: Date, default: Date.now }, // Marca de tiempo de la entrada, salida o descanso
+  marcaTiempo: { 
+    type: Date, 
+    default: Date.now,
+    get: timestamp => {
+      const options = {
+        year: 'numeric', month: '2-digit', day: '2-digit',
+        hour: '2-digit', minute: '2-digit', second: '2-digit',
+        hour12: false // Formato de 24 horas
+      };
+      return new Date(timestamp).toLocaleString('es-ES', options);
+    }
+  }, // Marca de tiempo de la entrada, salida o descanso
 });
 
 const Registro = mongoose.model("Registro", registroSchema);
 
 export default Registro;
+
